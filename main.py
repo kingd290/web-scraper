50d8f90b19432236cb52b9f0de4c6726bd09d237
kd9s0 <zetazeros@protonmail.com>
2023-07-01 10:22:35 -0700

    Update main.py (#1)

---
+++ main.py
@@ -0,0 +1,17 @@
+from scraper import scraper
+from data import output
+
+def main():
+    # Fetch the web page content
+    url = 'https://uniswap.org'
+    html_content = scraper.fetch_page(url)
+
+    if html_content:
+        # Scrape the data
+        data = scraper.scrape_data(html_content)
+
+        # Store the data
+        output.save_data(data)
+
+if __name__ == '__main__':
+    main()
