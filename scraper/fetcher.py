ff902ef99023be5759709d01006edc80fb98740f
kd9s0 <zetazeros@protonmail.com>
2023-07-01 11:20:47 -0700

    ...

--- scraper/fetcher.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def fetch_page(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    else:
-        return None
--- scraper/parser.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-def parse_data(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    # Implement your parsing logic here to extract specific data
-    # ...
-
-    # Return the extracted data
-    return extracted_data
