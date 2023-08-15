ee1200ed4f3deaf6fbed85e1b4d905151a251e14
kd9s0 <zetazeros@protonmail.com>
2023-08-15 10:59:52 -0700

    log errors

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import argparse
 import logging
 from scraper import scraper
 from data import output
 
-def main():
+def main(url):
     # Set up logging
     logging.basicConfig(filename='scraping.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Fetch the web page content
-    url = 'https://uniswap.org'
     html_content = scraper.fetch_page(url)
 
     if html_content:
@@ -22,4 +22,8 @@ def main():
             logging.error(f"Error scraping or saving data: {str(e)}")
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Web Scraping Script')
+    parser.add_argument('--url', required=True, help='URL of the web page to scrape')
+    args = parser.parse_args()
+
+    main(args.url)
